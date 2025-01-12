36ab029dccab02cfc1f262cc26b9e4fbf5e2cf6d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-01-12 15:32:15 +0530

    Create the `install-app` button

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { manifest } from './src/utility/manifest';
 import { seoConfig } from './src/utility/seoConfig';
 
+const IN_DEVELOPMENT = process.env.NODE_ENV != "PRODUCTION"
+
 // https://astro.build/config
 export default defineConfig({
     site: seoConfig.baseURL,
@@ -15,6 +17,9 @@ export default defineConfig({
             VitePWA({
                 registerType: 'autoUpdate',
                 manifest,
+                devOptions: {
+                    enabled: IN_DEVELOPMENT
+                },
                 workbox: {
                     globDirectory: 'dist',
                     // globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
