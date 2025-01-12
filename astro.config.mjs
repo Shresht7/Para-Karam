55f034758d6f366416cb27ab8e3acf4b3e4f8ed7
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-01-12 13:15:30 +0530

    Integrate plugin with astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,29 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from "@astrojs/sitemap";
+import { VitePWA } from 'vite-plugin-pwa';
+
+import site from "@src/data/metadata.json";
+import { manifest } from '@src/utility/manifest';
+import { seoConfig } from '@src/utility/seoConfig';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.para-karam.com",
-    integrations: [sitemap()]
+    site: site.baseURL,
+    integrations: [sitemap()],
+    vite: {
+        plugins: [
+            VitePWA({
+                registerType: 'autoUpdate',
+                manifest,
+                workbox: {
+                    globDirectory: 'dist',
+                    globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
+                    // Don't fallback on document based (e.g. `/some-page`) requests
+                    // This removes an errant console.log message from showing up.
+                    navigateFallback: null,
+                }
+            })
+        ]
+    }
 });
